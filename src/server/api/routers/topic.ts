60aaa5abad6c2cbb7c0cba33c70761389656b085
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-03-22 22:09:28 +0800

    creating schemas

---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,11 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session?.user.id,
+      },
+    });
+  }),
+});
